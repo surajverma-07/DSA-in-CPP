dff49466b7de8a276feb07cb6ad3db0c4b242ed1
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-14 07:10:22 +0530

    Homework question

--- lec7SwitchCase.cpp
+++ lec7SwitchCase.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
 int main(){
@@ -6,25 +7,41 @@ int main(){
     int num=1;
 
   cout<<endl;
+
   //Nested Switch case 
-  switch (ch)
-  {
-  case '3':
-     cout<<"character 3 \n";
-    break;
-  case '2':
-     cout<<"character 2 \n";
-    break;
-  case '1':
-      cout<<"character 1 \n";
-       switch(num){
-        case 1:
-           cout<<"Num is 1"<<endl;
-       }
-    break;
+//   switch (ch)
+//   {
+//   case '3':
+//      cout<<"character 3 \n";
+//     break;
+//   case '2':
+//      cout<<"character 2 \n";
+//     break;
+//   case '1':
+//       cout<<"character 1 \n";
+//        switch(num){
+//         case 1:
+//            cout<<"Num is 1"<<endl;
+//        }
+//     break;
   
-  default:
-       cout<<'kuch ni ';
-    break;
+//   default:
+//        cout<<'kuch ni ';
+//     break;
+//   }
+  
+
+  //Homework infinte loop and within it we have switch case how to tackle ?
+  while(1){
+    switch (num)
+    {
+    case 1:
+        cout<<"Case 1 Matched ";
+        break;
+    
+    default:
+        break;
+    }
+    exit(0);
   }
 }
