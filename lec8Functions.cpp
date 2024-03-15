a36a25549be5a011aa84c45409645a45e3414759
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-15 07:49:29 +0530

    prime number

--- lec8Functions.cpp
+++ lec8Functions.cpp
@@ -1,6 +1,6 @@
 // Function makes code easier simple and readable  
 // Using Function Reusability of code inhance we use multiple time 
-
+#include<cmath>
 #include<iostream>
 using namespace std;
 
@@ -31,12 +31,34 @@ int fact(int num){
     }
     return res;
 }
+
+
 int nCr(int n , int r){
     return(fact(n)/(fact(n-r)*fact(r)));
 }
 
 
+bool isPrime(int num){
+    if(num == 1) return false;
+    if(num == 2 || num ==3 || num == 5){
+        return true;
+    }
+    if(isEven(num)){
+        return false;
+    }
+    
+    for (int i = 2; i <= sqrt(num); i++)
+        {
+            if(num%i==0){
+                return false;
+            }
+        }
+    return true;        
+    
+}
+
 int main(){
+    int num;
     // int a,b;
     // cout<<"Enter a and b to calculate Pow(a,b)  ";
     // cin>>a>>b;
@@ -49,14 +71,23 @@ int main(){
 // To calculate nCr by taking input n & r 
 // Factorial
 
-int n,r;
-cout<<"\n***  Program to Calculate nCr  ***\n";
-cout<<"Enter n =  ";
-cin>>n;
-cout<<"Enter r =  ";
-cin>>r;
+// int n,r;
+// cout<<"\n***  Program to Calculate nCr  ***\n";
+// cout<<"Enter n =  ";
+// cin>>n;
+// cout<<"Enter r =  ";
+// cin>>r;
 
-int result = nCr(n,r);
-cout<<n<<"C"<<r<<"  =  "<<result<<endl;
+// int result = nCr(n,r);
+// cout<<n<<"C"<<r<<"  =  "<<result<<endl;
 
+cout<<"Enter nubmer to check wheather it is prime or not  ";
+cin>>num;
+bool res = isPrime(num);
+if(res){
+    cout<<num<<" is a prime number "<<endl;
+}else{
+    cout<<num<<" is not a prime number "<<endl;
 }
+}
+
