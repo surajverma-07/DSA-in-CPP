441b8cf8d0b350f96545b3f7fd273edd02f9965a
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-15 07:14:57 +0530

    To calculate nCr

--- lec8Functions.cpp
+++ lec8Functions.cpp
@@ -20,12 +20,43 @@ bool isEven(int num){
     else return true;
 }
 
+int fact(int num){
+    if(num == 0 || num ==1){
+        return 1;
+    }
+    int res = 1;
+    for (int  i = 1; i <= num; i++)
+    {
+        res = res*i;
+    }
+    return res;
+}
+int nCr(int n , int r){
+    return(fact(n)/(fact(n-r)*fact(r)));
+}
+
+
 int main(){
-    int a,b;
-    cout<<"Enter a and b to calculate Pow(a,b)  ";
-    cin>>a>>b;
+    // int a,b;
+    // cout<<"Enter a and b to calculate Pow(a,b)  ";
+    // cin>>a>>b;
 
     // cout<<a<<" exponent "<<b<<"  is =  "<<Power(a,b)<<endl;
-    cout<<a<<" is "<<((isEven(a))?("an even number \n"):("an odd number \n"));
-    cout<<b<<" is "<<((isEven(b))?("an even number \n"):("an odd number \n"));
+    // cout<<a<<" is "<<((isEven(a))?("an even number \n"):("an odd number \n"));
+    // cout<<b<<" is "<<((isEven(b))?("an even number \n"):("an odd number \n"));
+
+
+// To calculate nCr by taking input n & r 
+// Factorial
+
+int n,r;
+cout<<"\n***  Program to Calculate nCr  ***\n";
+cout<<"Enter n =  ";
+cin>>n;
+cout<<"Enter r =  ";
+cin>>r;
+
+int result = nCr(n,r);
+cout<<n<<"C"<<r<<"  =  "<<result<<endl;
+
 }
