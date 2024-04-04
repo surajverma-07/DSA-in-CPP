2526cf47ca9eba7ab79eb7b72a30e07342cf8e15
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-04-04 09:17:31 +0530

    sqrt using binary search

--- lec11Binary_Advance.cpp
+++ lec11Binary_Advance.cpp
@@ -21,10 +21,30 @@ int pivotElement(int arr[],int size){
     
 }
 
+long long int sqrt(int num){
+       long long int left = 0,right=num,ans,mid;
+        while(left<=right){
+            mid = left + (right-left)/2 ;
+            if(mid*mid == num){
+                return mid;
+            }
+            else if(mid*mid > num){
+                right = mid-1;
+            }
+            else if(mid*mid < num){
+                ans = mid;
+                left= mid+1;
+            }
+        }
+        return ans;
+    }
+    
+
 int main(){
 
     int arr[7] = { 7,9,12,2,4,5,6};
     
-    cout<<"Pivot elment in the array is =  "<<pivotElement(arr,7)<<endl;
+    // cout<<"Pivot elment in the array is =  "<<pivotElement(arr,7)<<endl;
+    cout<<"square root of 16 is  =  "<<sqrt(16);
     return 0;
 }
