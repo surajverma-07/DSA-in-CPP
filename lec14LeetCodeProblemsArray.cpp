897f0754d0b7c38d99dca93b46b0b55bf5380a79
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-05-18 08:44:57 +0530

    merging two sorted array

--- lec14LeetCodeProblemsArray.cpp
+++ lec14LeetCodeProblemsArray.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+void printArray(vector<int> v){
+    for(int i : v){
+        cout<<i<<"  ";
+    }
+    cout<<endl;
+}
+//Reverse Array
+/*
 vector<int> reverseVector(vector<int> v){
     int size = v.size();
     int n=size-1;
@@ -27,4 +35,58 @@ int main(){
         cout<<endl<<i<<"  ";
     }
     cout<<endl;
+}
+https://www.naukri.com/code360/problems/reverse-the-array_1262298?leftPanelTabValue=SUBMISSION
+*/
+
+//Merge Sorted Array 
+vector<int> mergeArray(vector<int> arr1 , vector<int> arr2){
+    int size1 = arr1.size();
+    int size2 = arr2.size();
+    vector<int> merge(size1+size2);
+    int i=0,j=0,k=0;
+    while (i<size1 && j<size2)
+    {
+      if(arr1[i]<arr2[j]){
+        merge[k] = arr1[i];
+        i++;
+        k++;
+      }
+      else{
+        merge[k] = arr2[j];
+        j++;
+        k++;
+      }
+    }
+    while (i < size1)
+    {
+        merge[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while (j < size2)
+    {
+        merge[k] = arr1[j];
+        j++;
+        k++;
+    }
+    return merge;
+}
+
+int main(){
+    vector<int> v1 ;
+    vector<int> v2;
+    v1.push_back(1);
+    v1.push_back(3);
+    v1.push_back(5);
+    v1.push_back(7);
+    v1.push_back(9);
+    v2.push_back(2);
+    v2.push_back(4);
+    v2.push_back(6);
+    v2.push_back(8);
+
+    vector<int> v_ans = mergeArray(v1,v2);
+    printArray(v_ans);
+
 }
